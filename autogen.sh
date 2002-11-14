1df5850c96111708350d1dcbbc8db86285ed9a8a
Simon Josefsson <simon@josefsson.org>
2002-11-14 09:29:46 +0000

    Use autoreconf.

--- autogen.sh
+++ autogen.sh
@@ -1,27 +1,3 @@
-#!/bin/sh
-set -x
-ACLOCAL=${ACLOCAL:-aclocal}; export ACLOCAL
-AUTOMAKE=${AUTOMAKE:-automake}; export AUTOMAKE
-AUTOCONF=${AUTOCONF:-autoconf}; export AUTOCONF
-LIBTOOLIZE=${LIBTOOLIZE:-libtoolize}; export LIBTOOLIZE
-AUTOHEADER=${AUTOHEADER:-autoheader}; export AUTOHEADER
-GETTEXTIZE=${GETTEXTIZE:-gettextize}; export GETTEXTIZE
-
-cd argp &&
-rm -vf config.cache &&
-rm -rvf autom4te.cache &&
-$ACLOCAL &&
-$AUTOCONF &&
-$AUTOMAKE --add-missing &&
-$AUTOHEADER &&
-cd .. &&
-rm -vf config.cache &&
-rm -rvf autom4te.cache &&
-$GETTEXTIZE --copy --intl --force &&
-$ACLOCAL -I m4 -I argp
-$LIBTOOLIZE --force --automake
-$ACLOCAL -I m4 -I argp
-$AUTOCONF
-$AUTOMAKE --gnits --add-missing
-$AUTOHEADER
+#!/bin/sh -x
+autoreconf --install --force --verbose
 : 'You can now run CFLAGS="-g -Wall" ./configure --enable-maintainer-mode and then make.'
