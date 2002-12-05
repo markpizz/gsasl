1d33a5564943b48079dad853474ed370c69a27a2
Simon Josefsson <simon@josefsson.org>
2002-12-05 00:07:47 +0000

    fix

--- autogen.sh
+++ autogen.sh
@@ -1,3 +1,3 @@
 #!/bin/sh -x
 autoreconf --install --force --verbose
-: 'You can now run CFLAGS="-g -Wall" ./configure --enable-maintainer-mode and then make.'
+: 'You can now run ./configure CFLAGS="-g -Wall" --enable-maintainer-mode and then make.'
