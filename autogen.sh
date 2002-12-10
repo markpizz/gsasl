7f5b412f7ee496696a4c93498748f0bec38b80d8
Simon Josefsson <simon@josefsson.org>
2002-12-10 14:48:51 +0000

    *** empty log message ***

--- autogen.sh
+++ autogen.sh
@@ -1,3 +1,3 @@
 #!/bin/sh -x
 autoreconf --install --force --verbose
-: 'You can now run ./configure CFLAGS="-g -Wall" --enable-maintainer-mode and then make.'
+: './configure CFLAGS="-g -Wall -pedantic" --enable-maintainer-mode --with-dmalloc --disable-shared'
