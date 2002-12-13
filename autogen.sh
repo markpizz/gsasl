8247faade4a5abb9ee8ecc400960589c70c001f6
Simon Josefsson <simon@josefsson.org>
2002-12-13 12:50:14 +0000

    *** empty log message ***

--- autogen.sh
+++ autogen.sh
@@ -1,3 +1,6 @@
 #!/bin/sh -x
 autoreconf --install --force --verbose
-: './configure CFLAGS="-g -Wall -pedantic" --enable-maintainer-mode --with-dmalloc --disable-shared'
+./configure CFLAGS="-g -Wall -W -Wtraditional -Wundef -Wpointer-arith -Wbad-function-cast -Wcast-align -Wsign-compare -Waggregate-return -Wstrict-prototypes -Wmissing-prototypes -Wmissing-declarations -Wmissing-noreturn -Wnested-externs -Winline" --enable-maintainer-mode --with-dmalloc "$@"
+# maybe add: -Wconversion
+# perhaps add: -Wwrite-strings
+# bad: -Wredundant-decls -Wshadow
