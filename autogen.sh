4929ea64ce968c0948451171db4d682f5e0f8bab
Simon Josefsson <simon@josefsson.org>
2003-02-03 12:14:59 +0000

    Cleanup.

--- autogen.sh
+++ autogen.sh
@@ -1,6 +1,2 @@
 #!/bin/sh -x
 autoreconf --install --force --verbose
-./configure CFLAGS="-g -Wall -W -Wtraditional -Wundef -Wpointer-arith -Wbad-function-cast -Wcast-align -Wsign-compare -Waggregate-return -Wstrict-prototypes -Wmissing-prototypes -Wmissing-declarations -Wmissing-noreturn -Wnested-externs -Winline" --enable-maintainer-mode --with-dmalloc "$@"
-# maybe add: -Wconversion
-# perhaps add: -Wwrite-strings
-# bad: -Wredundant-decls -Wshadow
