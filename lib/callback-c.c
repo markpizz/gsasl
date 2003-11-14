1cfe1ae1e27dce366d54c891a2a903fb38922063
Simon Josefsson <simon@josefsson.org>
2003-11-14 06:23:37 +0000

    (gsasl_client_ctx_get): Move to obsolete.c (use gsasl_ctx_get instead). (gsasl_client_application_data_get): Move to obsolete.c (use gsasl_appinfo_get instead). (gsasl_client_application_data_set): Move to obsolete.c (use gsasl_appinfo_set instead).

--- lib/callback-c.c
+++ lib/callback-c.c
@@ -22,53 +22,6 @@
 #include "internal.h"
 
 /**
- * gsasl_client_ctx_get:
- * @sctx: libgsasl client handle
- *
- * Return value: Returns the libgsasl handle given a libgsasl client handle.
- **/
-Gsasl_ctx *
-gsasl_client_ctx_get (Gsasl_session_ctx * sctx)
-{
-  return sctx->ctx;
-}
-
-/**
- * gsasl_client_application_data_set:
- * @sctx: libgsasl client handle.
- * @application_data: opaque pointer to application specific data.
- *
- * Store application specific data in the libgsasl client handle.  The
- * application data can be later (for instance, inside a callback) be
- * retrieved by calling gsasl_client_application_data_get().  It is
- * normally used by the application to maintain state between the main
- * program and the callback.
- **/
-void
-gsasl_client_application_data_set (Gsasl_session_ctx * sctx,
-				   void *application_data)
-{
-  sctx->application_data = application_data;
-}
-
-/**
- * gsasl_client_application_data_get:
- * @sctx: libgsasl client handle.
- *
- * Retrieve application specific data from libgsasl client handle. The
- * application data is set using gsasl_client_application_data_set().
- * It is normally used by the application to maintain state between
- * the main program and the callback.
- *
- * Return value: Returns the application specific data, or NULL.
- **/
-void *
-gsasl_client_application_data_get (Gsasl_session_ctx * sctx)
-{
-  return sctx->application_data;
-}
-
-/**
  * gsasl_client_callback_authentication_id_set:
  * @ctx: libgsasl handle.
  * @cb: callback function
