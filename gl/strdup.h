9e6384adf9400f8eacf800f490881901ba536c6d
Simon Josefsson <simon@josefsson.org>
2004-01-18 19:44:35 +0000

    Fix.

--- gl/strdup.h
+++ gl/strdup.h
@@ -1,2 +1,5 @@
+#include <string.h>
+#if defined HAVE_DECL_STRDUP && !HAVE_DECL_STRDUP && ! defined strdup
 /* Duplicate S, returning an identical malloc'd string.  */
 char *strdup (const char *s);
+#endif
