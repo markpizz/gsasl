b7cae2fe899bd4ebf3f6dd32383da114cc9092a1
Simon Josefsson <simon@josefsson.org>
2004-01-18 07:05:14 +0000

    Add crypto.h.

---
+++ lib/crypto.h
@@ -0,0 +1,23 @@
+/* crypto.h --- Prototypes for crypto files (nettle.c, libgcrypt.c, ...).
+ * Copyright (C) 2002, 2003  Simon Josefsson
+ *
+ * This file is part of GNU SASL.
+ *
+ * GNU SASL is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * GNU SASL is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public
+ * License along with GNU SASL; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ *
+ */
+
+/* See nettle.c, libgcrypt.c, ... */
+extern int _gsasl_crypto_init (void);
