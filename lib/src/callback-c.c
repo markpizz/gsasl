b49d361c806350c229063b3cf284eb54edc630ed
Simon Josefsson <simon@josefsson.org>
2004-04-15 22:02:50 +0000

    Fix license.

--- lib/src/callback-c.c
+++ lib/src/callback-c.c
@@ -1,20 +1,20 @@
-/* callback-c.c	callback handling (for clients)
- * Copyright (C) 2002, 2003  Simon Josefsson
+/* callback-c.c	--- Callback handling (for clients).
+ * Copyright (C) 2002, 2003, 2004  Simon Josefsson
  *
- * This file is part of GNU SASL.
+ * This file is part of GNU SASL Library.
  *
- * GNU SASL is free software; you can redistribute it and/or
- * modify it under the terms of the GNU General Public
- * License as published by the Free Software Foundation; either
- * version 2.1 of the License, or (at your option) any later version.
+ * GNU SASL Library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public License
+ * as published by the Free Software Foundation; either version 2.1 of
+ * the License, or (at your option) any later version.
  *
- * GNU SASL is distributed in the hope that it will be useful,
+ * GNU SASL Library is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
- * General Public License for more details.
+ * Lesser General Public License for more details.
  *
- * You should have received a copy of the GNU General Public
- * License along with GNU SASL; if not, write to the Free Software
+ * You should have received a copy of the GNU Lesser General Public License
+ * License along with GNU SASL Library; if not, write to the Free Software
  * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
  *
  */
