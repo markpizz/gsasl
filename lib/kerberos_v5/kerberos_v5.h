3da90a455b52560290beb44e7b6c988062e369bb
Simon Josefsson <simon@josefsson.org>
2004-04-15 21:39:03 +0000

    Fix license.

--- lib/kerberos_v5/kerberos_v5.h
+++ lib/kerberos_v5/kerberos_v5.h
@@ -1,21 +1,22 @@
-/* kerberos_v5.h	header file for experimental SASL mechanism KERBEROS_V5
- * Copyright (C) 2003  Simon Josefsson
+/* kerberos_v5.h --- Prototypes for experimental SASL mechanism KERBEROS_V5.
+ * Copyright (C) 2003, 2004  Simon Josefsson
  *
- * This file is part of GNU SASL.
+ * This file is part of GNU SASL Library.
  *
- * GNU SASL is free software; you can redistribute it and/or
- * modify it under the terms of the GNU General Public
- * License as published by the Free Software Foundation; either
- * version 2.1 of the License, or (at your option) any later version.
+ * GNU SASL Library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public License
+ * as published by the Free Software Foundation; either version 2.1 of
+ * the License, or (at your option) any later version.
  *
- * GNU SASL is distributed in the hope that it will be useful,
+ * GNU SASL Library is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
- * General Public License for more details.
+ * Lesser General Public License for more details.
  *
- * You should have received a copy of the GNU General Public
- * License along with GNU SASL; if not, write to the Free Software
- * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with GNU SASL Library; if not, write to the Free
+ * Software Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA
+ * 02111-1307 USA
  *
  */
 
