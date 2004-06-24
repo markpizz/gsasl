1e17c6919e2607f62467dc7e31a4bfaf30338ac2
Simon Josefsson <simon@josefsson.org>
2004-06-24 18:34:19 +0000

    Add.

---
+++ tests/cram-md5.c
@@ -0,0 +1,121 @@
+/* cram-md5.c --- Test the CRAM-MD5 mechanism.
+ * Copyright (C) 2002, 2003, 2004  Simon Josefsson
+ *
+ * This file is part of GNU SASL.
+ *
+ * GNU SASL is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * GNU SASL is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with GNU SASL; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ *
+ */
+
+#ifdef HAVE_CONFIG_H
+# include "config.h"
+#endif
+
+#include <stdio.h>
+#include <stdarg.h>
+#include <stdlib.h>
+#include <string.h>
+
+#include "utils.h"
+
+#define PASSWORD "sesame"
+#define USERNAME "foo bar"
+
+static int
+server_cb_retrieve (Gsasl_session_ctx * xctx,
+		    const char *authentication_id,
+		    const char *authorization_id,
+		    const char *realm, char *key, size_t * keylen)
+{
+  if (*keylen < strlen (PASSWORD))
+    return GSASL_TOO_SMALL_BUFFER;
+
+  *keylen = strlen (PASSWORD);
+  if (key)
+    memcpy (key, PASSWORD, *keylen);
+
+  return GSASL_OK;
+}
+
+static int
+client_cb_authentication_id (Gsasl_session_ctx * xctx,
+			     char *out, size_t * outlen)
+{
+  if (*outlen < strlen (USERNAME))
+    return GSASL_TOO_SMALL_BUFFER;
+
+  *outlen = strlen (USERNAME);
+  if (out)
+    memcpy (out, USERNAME, *outlen);
+
+  return GSASL_OK;
+}
+
+static int
+client_cb_password (Gsasl_session_ctx * xctx, char *out, size_t * outlen)
+{
+  if (*outlen < strlen (PASSWORD))
+    return GSASL_TOO_SMALL_BUFFER;
+
+  *outlen = strlen (PASSWORD);
+  if (out)
+    memcpy (out, PASSWORD, *outlen);
+
+  return GSASL_OK;
+}
+
+void
+doit (void)
+{
+  Gsasl_ctx *ctx = NULL;
+  Gsasl_session_ctx *server = NULL, *client = NULL;
+  char *output;
+  size_t outputlen;
+  size_t i, j;
+  int res;
+
+  res = gsasl_init (&ctx);
+  if (res != GSASL_OK)
+    {
+      fail ("gsasl_init() failed (%d):\n%s\n", res, gsasl_strerror (res));
+      return;
+    }
+
+  gsasl_server_callback_retrieve_set (ctx, server_cb_retrieve);
+
+  res = gsasl_server_start (ctx, "CRAM-MD5", &server);
+  if (res != GSASL_OK)
+    {
+      fail ("gsasl_init() failed (%d):\n%s\n", res, gsasl_strerror (res));
+      return;
+    }
+
+  gsasl_client_callback_authentication_id_set (ctx,
+					       client_cb_authentication_id);
+  gsasl_client_callback_password_set (ctx, client_cb_password);
+
+  res = gsasl_client_start (ctx, "CRAM-MD5", &client);
+  if (res != GSASL_OK)
+    {
+      fail ("gsasl_init() failed (%d):\n%s\n", res, gsasl_strerror (res));
+      return;
+    }
+
+
+
+  gsasl_client_finish (client);
+  gsasl_server_finish (server);
+  gsasl_done (ctx);
+}
