8dae6b76d570dd1b5431294798f3a28c60cf3f8b
Simon Josefsson <simon@josefsson.org>
2004-08-31 17:27:15 +0000

    Fix example.

--- doc/gsasl-example1.c
+++ doc/gsasl-example1.c
@@ -3,35 +3,35 @@
 #include <gsasl.h>
 
 /* Build using the following command:
- * gcc -o foo foo.c `libgsasl-config --cflags --libs`
+ * gcc gsasl-example1.c `pkg-config --cflags --libs libgsasl`
  */
 
 int
 main (int argc, char *argv[])
-@{
+{
   Gsasl_ctx *ctx;
   int res;
 
   setlocale (LC_ALL, "");
 
   if (gsasl_check_version(GSASL_VERSION) == NULL)
-    @{
+    {
       fprintf(stderr, "Libgsasl is %s expected %s\n",
 	      gsasl_check_version(NULL), GSASL_VERSION);
       return 1;
-    @}
-  
+    }
+
   res = gsasl_init (&ctx);
   if (res != GSASL_OK)
-    @{
-      fprintf(stderr, "Cannot initialize libgsasl: %s\n", 
+    {
+      fprintf(stderr, "Cannot initialize libgsasl: %s\n",
 	      gsasl_strerror(res));
       return 1;
-    @}
+    }
 
   /* Do things here ... */
 
   gsasl_done(ctx);
 
   return 0;
-@}
+}
