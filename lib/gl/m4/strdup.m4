8e4a5d078c5793fca4395b9ea45ffb4a57d9e9d1
Simon Josefsson <simon@josefsson.org>
2004-09-28 23:17:23 +0000

    Use new GNULib.

--- lib/gl/m4/strdup.m4
+++ lib/gl/m4/strdup.m4
@@ -1,4 +1,4 @@
-# strdup.m4 serial 5
+# strdup.m4 serial 4
 dnl Copyright (C) 2002, 2003, 2004 Free Software Foundation, Inc.
 dnl This file is free software, distributed under the terms of the GNU
 dnl General Public License.  As a special exception to the GNU General
@@ -10,7 +10,6 @@ AC_DEFUN([gl_FUNC_STRDUP],
 [
   AC_REPLACE_FUNCS(strdup)
   AC_CHECK_DECLS_ONCE(strdup)
-  AH_BOTTOM([#include <strdup.h>])
   gl_PREREQ_STRDUP
 ])
 
--- lib/gl/strdup.c
+++ lib/gl/strdup.c
@@ -1,34 +1,44 @@
-/* Copyright (C) 1991, 1996, 1997, 1998, 2002 Free Software Foundation, Inc.
-   Copyright (C) 2004 Simon Josefsson
+/* Copyright (C) 1991, 1996, 1997, 1998, 2002, 2003, 2004 Free Software
+   Foundation, Inc.
 
-   The file is free software; you can redistribute it and/or
-   modify it under the terms of the GNU Lesser General Public
-   License as published by the Free Software Foundation; either
-   version 2.1 of the License, or (at your option) any later version.
+   This file is part of the GNU C Library.
 
-   This file is distributed in the hope that it will be useful,
+   This program is free software; you can redistribute it and/or modify
+   it under the terms of the GNU Lesser General Public License as published by
+   the Free Software Foundation; either version 2.1, or (at your option)
+   any later version.
+
+   This program is distributed in the hope that it will be useful,
    but WITHOUT ANY WARRANTY; without even the implied warranty of
-   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
-   Lesser General Public License for more details.
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU Lesser General Public License for more details.
 
-   You should have received a copy of the GNU Lesser General Public
-   License along with this file; if not, write to the Free Software
-   Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307
-   USA.  */
+   You should have received a copy of the GNU Lesser General Public License along
+   with this program; if not, write to the Free Software Foundation,
+   Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.  */
 
 #ifdef HAVE_CONFIG_H
 # include <config.h>
 #endif
 
-/* Get malloc. */
-#include <stdlib.h>
+#ifndef _LIBC
+/* Get specification.  */
+# include "strdup.h"
+#endif
 
-/* Get strlen, memcpy. */
+#include <stdlib.h>
 #include <string.h>
+
+#undef __strdup
+#undef strdup
+
+#ifndef weak_alias
+# define __strdup strdup
+#endif
 
 /* Duplicate S, returning an identical malloc'd string.  */
 char *
-strdup (const char *s)
+__strdup (const char *s)
 {
   size_t len = strlen (s) + 1;
   void *new = malloc (len);
@@ -38,3 +48,9 @@ strdup (const char *s)
 
   return (char *) memcpy (new, s, len);
 }
+#ifdef libc_hidden_def
+libc_hidden_def (__strdup)
+#endif
+#ifdef weak_alias
+weak_alias (__strdup, strdup)
+#endif
