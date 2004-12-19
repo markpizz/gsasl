7c2bc01eeb6242a30636812a152fd01218ba4302
Simon Josefsson <simon@josefsson.org>
2004-12-19 03:11:15 +0000

    Add.

---
+++ lib/digest-md5/validate.h
@@ -0,0 +1,32 @@
+/* validate.h --- Validate consistency of DIGEST-MD5 tokens.
+ * Copyright (C) 2004  Simon Josefsson
+ *
+ * This file is part of GNU SASL Library.
+ *
+ * GNU SASL Library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public License
+ * as published by the Free Software Foundation; either version 2.1 of
+ * the License, or (at your option) any later version.
+ *
+ * GNU SASL Library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with GNU SASL Library; if not, write to the Free
+ * Software Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA
+ * 02111-1307 USA
+ *
+ */
+
+#ifndef DIGEST_MD5_VALIDATE_H
+# define DIGEST_MD5_VALIDATE_H
+
+/* Get token types. */
+#include "tokens.h"
+
+extern int digest_md5_validate (digest_md5_challenge *c,
+				digest_md5_response *r);
+
+#endif /* DIGEST_MD5_VALIDATE_H */
