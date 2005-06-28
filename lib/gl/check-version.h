63f542fff5be8ecde97c78019f35c89fed8ba6e2
Simon Josefsson <simon@josefsson.org>
2005-06-28 09:55:56 +0000

    Use the real check-version module.

--- lib/gl/check-version.h
+++ lib/gl/check-version.h
@@ -1,4 +1,4 @@
-/* check_version.h --- Check version string compatibility.
+/* check-version.h --- Check version string compatibility.
    Copyright (C) 2005 Free Software Foundation, Inc.
 
    This program is free software; you can redistribute it and/or modify
