202c74046eb778b840444cd08589a59fe6e8c289
Simon Josefsson <simon@josefsson.org>
2008-07-01 16:03:03 +0200

    Fix nl_langinfo error.

--- src/callbacks.c
+++ src/callbacks.c
@@ -1,5 +1,5 @@
 /* callbacks.c --- Implementation of gsasl callbacks.
- * Copyright (C) 2002, 2003, 2004, 2005, 2006, 2007  Simon Josefsson
+ * Copyright (C) 2002, 2003, 2004, 2005, 2006, 2007, 2008  Simon Josefsson
  *
  * This file is part of GNU SASL.
  *
@@ -23,6 +23,8 @@
 
 #include "striconv.h"
 #include "readline.h"
+
+#include <langinfo.h> /* For nl_langinfo. */
 
 static char *
 locale_to_utf8 (char *str)
