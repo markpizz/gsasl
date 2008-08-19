157db3de6c65dc278cca37ac00680865b02d8083
Simon Josefsson <simon@josefsson.org>
2008-08-19 22:13:55 +0200

    Update license. See discussion in <http://news.gmane.org/find-root.php?message_id=%3c824CED64%2dD5F3%2d4626%2dA030%2d3D5A1F25663B%40java.pl%3e> and <http://news.gmane.org/find-root.php?message_id=%3cBB01BC68%2dA998%2d4E91%2d9501%2d7691BB7BBFC3%40java.pl%3e>.

--- lib/win32/include/gen-win32-headers.pl
+++ lib/win32/include/gen-win32-headers.pl
@@ -4,20 +4,25 @@
 #
 # Author: Adam Strzelecki <ono@java.pl>.
 #
-# This file is part of GNU Libidn.
+# This file is part of GNU SASL Library.
 #
-# This program is free software: you can redistribute it and/or modify
-# it under the terms of the GNU General Public License as published by
-# the Free Software Foundation, either version 3 of the License, or
-# (at your option) any later version.
+# GNU SASL Library is free software; you can redistribute it and/or
+# modify it under the terms of the GNU Lesser General Public License
+# as published by the Free Software Foundation; either version 2.1 of
+# the License, or (at your option) any later version.
 #
-# This program is distributed in the hope that it will be useful,
+# GNU SASL Library is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU General Public License for more details.
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+# Lesser General Public License for more details.
 #
-# You should have received a copy of the GNU General Public License
-# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+# You should have received a copy of the GNU Lesser General Public
+# License along with GNU SASL Library; if not, write to the Free
+# Software Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+# MA 02110-1301, USA.
+#
+# I consider the output of this program to be unrestricted.  Use it as
+# you will.
 
 # This script generates win32 (Visual Studio) include files for libidn
 # using configure.ac.
