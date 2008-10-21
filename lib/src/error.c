aa07804245b3e91c58166d0dca96735f8017c51e
Simon Josefsson <simon@josefsson.org>
2008-10-21 13:10:12 +0200

    Make it work with --disable-obsolete.

--- lib/src/error.c
+++ lib/src/error.c
@@ -28,12 +28,12 @@
 #define gettext_noop(String) String
 #define N_(String) gettext_noop (String)
 
-#define ERR(name, desc)	{ name, #name, desc }
+#define ERR(name, desc) { name, #name, desc }
 
 #ifdef GSASL_NO_OBSOLETE
-#define OBS(name, desc) { name, NULL, NULL }
+#define OBS(i, name, desc) { i, NULL, NULL }
 #else
-#define OBS(name, desc) { name, #name, desc }
+#define OBS(i, name, desc) { name, #name, desc }
 #endif
 
 static struct {
@@ -46,75 +46,76 @@ static struct {
   ERR (GSASL_UNKNOWN_MECHANISM, N_("Unknown SASL mechanism")),
   ERR (GSASL_MECHANISM_CALLED_TOO_MANY_TIMES,
        N_("SASL mechanism called too many times")),
-  OBS (GSASL_TOO_SMALL_BUFFER,
+  OBS (4, GSASL_TOO_SMALL_BUFFER,
        N_("SASL function needs larger buffer (internal error)")),
-  OBS (GSASL_FOPEN_ERROR, N_("Could not open file in SASL library")),
-  OBS (GSASL_FCLOSE_ERROR, N_("Could not close file in SASL library")),
+  OBS (5, GSASL_FOPEN_ERROR, N_("Could not open file in SASL library")),
+  OBS (6, GSASL_FCLOSE_ERROR, N_("Could not close file in SASL library")),
   ERR (GSASL_MALLOC_ERROR, N_("Memory allocation error in SASL library")),
   ERR (GSASL_BASE64_ERROR, N_("Base 64 coding error in SASL library")),
   ERR (GSASL_CRYPTO_ERROR, N_("Low-level crypto error in SASL library")),
   { 10, NULL, NULL },
-  OBS (GSASL_NEED_CLIENT_ANONYMOUS_CALLBACK,
+  OBS (11, GSASL_NEED_CLIENT_ANONYMOUS_CALLBACK,
        N_("SASL mechanism needs gsasl_client_callback_anonymous() callback"
 	  " (application error)")),
-  OBS (GSASL_NEED_CLIENT_PASSWORD_CALLBACK,
+  OBS (12, GSASL_NEED_CLIENT_PASSWORD_CALLBACK,
        N_("SASL mechanism needs gsasl_client_callback_password() callback"
 	  " (application error)")),
-  OBS (GSASL_NEED_CLIENT_PASSCODE_CALLBACK,
+  OBS (13, GSASL_NEED_CLIENT_PASSCODE_CALLBACK,
        N_("SASL mechanism needs gsasl_client_callback_passcode() callback"
 	  " (application error)")),
-  OBS (GSASL_NEED_CLIENT_PIN_CALLBACK,
+  OBS (14, GSASL_NEED_CLIENT_PIN_CALLBACK,
        N_("SASL mechanism needs gsasl_client_callback_pin() callback"
 	  " (application error)")),
-  OBS (GSASL_NEED_CLIENT_AUTHORIZATION_ID_CALLBACK,
+  OBS (15, GSASL_NEED_CLIENT_AUTHORIZATION_ID_CALLBACK,
        N_("SASL mechanism needs gsasl_client_callback_authorization_id() "
 	  "callback (application error)")),
-  OBS (GSASL_NEED_CLIENT_AUTHENTICATION_ID_CALLBACK,
+  OBS (16, GSASL_NEED_CLIENT_AUTHENTICATION_ID_CALLBACK,
        N_("SASL mechanism needs gsasl_client_callback_authentication_id() "
 	  "callback (application error)")),
-  OBS (GSASL_NEED_CLIENT_SERVICE_CALLBACK,
+  OBS (17, GSASL_NEED_CLIENT_SERVICE_CALLBACK,
        N_("SASL mechanism needs gsasl_client_callback_service() callback "
 	  "(application error)")),
-  OBS (GSASL_NEED_SERVER_VALIDATE_CALLBACK,
+  OBS (18, GSASL_NEED_SERVER_VALIDATE_CALLBACK,
        N_("SASL mechanism needs gsasl_server_callback_validate() callback "
 	  "(application error)")),
-  OBS (GSASL_NEED_SERVER_CRAM_MD5_CALLBACK,
+  OBS (19, GSASL_NEED_SERVER_CRAM_MD5_CALLBACK,
        N_("SASL mechanism needs gsasl_server_callback_cram_md5() callback "
 	  "(application error)")),
-  OBS (GSASL_NEED_SERVER_DIGEST_MD5_CALLBACK,
+  OBS (20, GSASL_NEED_SERVER_DIGEST_MD5_CALLBACK,
        N_("SASL mechanism needs gsasl_server_callback_digest_md5() callback "
 	  "(application error)")),
-  OBS (GSASL_NEED_SERVER_EXTERNAL_CALLBACK,
+  OBS (21, GSASL_NEED_SERVER_EXTERNAL_CALLBACK,
        N_("SASL mechanism needs gsasl_server_callback_external() callback "
 	  "(application error)")),
-  OBS (GSASL_NEED_SERVER_ANONYMOUS_CALLBACK,
+  OBS (22, GSASL_NEED_SERVER_ANONYMOUS_CALLBACK,
        N_("SASL mechanism needs gsasl_server_callback_anonymous() callback "
 	  "(application error)")),
-  OBS (GSASL_NEED_SERVER_REALM_CALLBACK,
+  OBS (23, GSASL_NEED_SERVER_REALM_CALLBACK,
        N_("SASL mechanism needs gsasl_server_callback_realm() callback "
 	  "(application error)")),
-  OBS (GSASL_NEED_SERVER_SECURID_CALLBACK,
+  OBS (24, GSASL_NEED_SERVER_SECURID_CALLBACK,
        N_("SASL mechanism needs gsasl_server_callback_securid() callback "
 	  "(application error)")),
-  OBS (GSASL_NEED_SERVER_SERVICE_CALLBACK,
+  OBS (25, GSASL_NEED_SERVER_SERVICE_CALLBACK,
        N_("SASL mechanism needs gsasl_server_callback_service() callback "
 	  "(application error)")),
-  OBS (GSASL_NEED_SERVER_GSSAPI_CALLBACK,
+  OBS (26, GSASL_NEED_SERVER_GSSAPI_CALLBACK,
        N_("SASL mechanism needs gsasl_server_callback_gssapi() callback "
 	  "(application error)")),
-  OBS (GSASL_NEED_SERVER_RETRIEVE_CALLBACK,
+  OBS (27, GSASL_NEED_SERVER_RETRIEVE_CALLBACK,
        N_("SASL mechanism needs gsasl_server_callback_retrieve() callback "
 	  "(application error)")),
-  OBS (GSASL_UNICODE_NORMALIZATION_ERROR,
+  OBS (28, GSASL_UNICODE_NORMALIZATION_ERROR,
        N_("Failed to perform Unicode Normalization on string.")),
   ERR (GSASL_SASLPREP_ERROR,
        N_("Could not prepare internationalized (non-ASCII) string.")),
-  ERR (GSASL_MECHANISM_PARSE_ERROR, N_("SASL mechanism could not parse input")),
+  ERR (GSASL_MECHANISM_PARSE_ERROR,
+       N_("SASL mechanism could not parse input")),
   ERR (GSASL_AUTHENTICATION_ERROR, N_("Error authenticating user")),
-  OBS (GSASL_CANNOT_GET_CTX,
+  OBS (32, GSASL_CANNOT_GET_CTX,
        N_("Cannot get internal library handle (library error)")),
   ERR (GSASL_INTEGRITY_ERROR, N_("Integrity error in application payload")),
-  OBS (GSASL_NO_MORE_REALMS, N_("No more realms available (non-fatal)")),
+  OBS (34, GSASL_NO_MORE_REALMS, N_("No more realms available (non-fatal)")),
   ERR (GSASL_NO_CLIENT_CODE,
        N_("Client-side functionality not available in library "
 	  "(application error)")),
@@ -162,7 +163,7 @@ static struct {
        N_("SecurID needs additional passcode.")),
   ERR (GSASL_SECURID_SERVER_NEED_NEW_PIN,
        N_("SecurID needs new pin.")),
-  OBS (GSASL_INVALID_HANDLE,
+  OBS (50, GSASL_INVALID_HANDLE,
        N_("The provided library handle was invalid (application error)")),
   ERR (GSASL_NO_CALLBACK,
        N_("No callback specified by caller (application error).")),
