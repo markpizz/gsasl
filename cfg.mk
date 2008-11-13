5735f3ee7956979f182190883684689a04f86df0
Simon Josefsson <simon@josefsson.org>
2008-11-13 19:27:53 +0100

    Fix warning handling.  Use output-def.m4.

--- cfg.mk
+++ cfg.mk
@@ -15,7 +15,7 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-CFGFLAGS ?= --enable-gtk-doc WARN_CFLAGS="-Werror -Wno-deprecated-declarations"
+CFGFLAGS ?= --enable-gtk-doc
 
 INDENT_SOURCES = `find . -name \*.c -or -name \*.h | grep -v -e /gl -e build-aux -e /win32/`
 
@@ -42,7 +42,7 @@ update-po: refresh-po
 	git-commit -m "Sync with TP." po/LINGUAS po/*.po.in
 
 bootstrap: autoreconf
-	./configure $(CFGFLAGS)
+	WARN_CFLAGS="-Werror -Wno-deprecated-declarations" ./configure $(CFGFLAGS)
 
 web-coverage:
 	rm -fv `find $(htmldir)/coverage -type f | grep -v CVS`
