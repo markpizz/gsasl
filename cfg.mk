919452ddaa56116ccc6cefd0876b8ed3dd37ff1a
Simon Josefsson <simon@josefsson.org>
2009-06-13 19:53:47 +0200

    Fix path.

--- cfg.mk
+++ cfg.mk
@@ -95,7 +95,7 @@ upload:
 web:
 	cd doc && env MAKEINFO="makeinfo -I ../examples" \
 		      TEXI2DVI="texi2dvi -I ../examples" \
-		$(build_aux)/gendocs.sh --html "--css-include=texinfo.css" \
+		../$(build_aux)/gendocs.sh --html "--css-include=texinfo.css" \
 		-o ../$(htmldir)/manual/ $(PACKAGE) "$(PACKAGE_NAME)"
 	cd doc/doxygen && doxygen && cd ../.. && cp -v doc/doxygen/html/* $(htmldir)/doxygen/ && cd doc/doxygen/latex && make refman.pdf && cd ../../../ && cp doc/doxygen/latex/refman.pdf $(htmldir)/doxygen/$(PACKAGE).pdf
 	cp -v doc/reference/html/*.html doc/reference/html/*.png doc/reference/html/*.devhelp doc/reference/html/*.css $(htmldir)/reference/
