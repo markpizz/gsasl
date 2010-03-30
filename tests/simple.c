4c72b81f1e5957dab98aa606c55c3cba0d19b5af
Simon Josefsson <simon@josefsson.org>
2010-03-30 16:46:59 +0200

    Check NULL handling in interfaces.

--- tests/simple.c
+++ tests/simple.c
@@ -1,5 +1,5 @@
 /* simple.c --- Test the simple SASL mechanisms.
- * Copyright (C) 2002, 2003, 2004, 2005, 2006, 2007, 2008, 2009  Simon Josefsson
+ * Copyright (C) 2002, 2003, 2004, 2005, 2006, 2007, 2008, 2009, 2010  Simon Josefsson
  *
  * This file is part of GNU SASL.
  *
@@ -415,4 +415,8 @@ doit (void)
     }
 
   gsasl_done (ctx);
+
+  /* Sanity check interfaces. */
+  gsasl_finish (NULL);
+  gsasl_done (NULL);
 }
